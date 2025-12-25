2a04a21f8394af129e1a713b76326d2183617e4f
hcurifer <hcurifer@myuax.com>
2025-12-26 00:41:24 +0100

    Rutas hijas
    Se crean todos los componentes de paginas y se añaden a las rutas hijas de app.routes.ts

---
+++ FactoryControl/src/app/features/maquinas/maquinas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maquinas',
+  imports: [],
+  templateUrl: './maquinas.component.html',
+  styleUrl: './maquinas.component.scss'
+})
+export class MaquinasComponent {
+
+}
