1e3f0f862262352a2e911838edbf30b4023049a2
hcurifer <hcurifer@myuax.com>
2025-12-25 15:00:15 +0100

    Ruta y Main-layout
    se crea la primera estructura de toolbar y el main debajo con la ruta que venga, se hace ruta hijo de dashboard para probar y dejar. Se verifica y OK

--- FactoryControl/src/app/app.routes.ts
+++ FactoryControl/src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: 'app',
     component: MainLayoutComponent,
-    children: []
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      }
+    ]
   },
   {
     path: '',
