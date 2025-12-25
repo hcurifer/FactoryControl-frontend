9c0cd6bb7c7f7e29ef5f35741bd70b81912770f8
hcurifer <hcurifer@myuax.com>
2025-12-26 00:05:27 +0100

    sidenav creado y verificado
    se crea componente sidenav, se incluye en mainlayout y se comprueba que se ve correcto con un placeholder

--- FactoryControl/src/app/core/layout/main-layout/main-layout.component.ts
+++ FactoryControl/src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet,
+    ToolbarComponent,
+    SidenavComponent
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
